89456e27ab11f8af9172519192aa64fe244e469f
bruno <brunocarvalhorodrigues@hotmail.com>
2025-12-02 08:31:25 -0300

    correcoes para a publicacao - contador

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,6 @@
 AOS.init();
 
-<<<<<<< HEAD
-
-const dataDoEvento = new Date(2025, 11, 6, 20, 0, 0);
-
-
+const dataDoEvento = new Date("Dec 06, 2025 20:00:00");
 const contador = document.getElementById('contador');
 
 function atualizaContador() {
@@ -23,37 +19,12 @@ function atualizaContador() {
 
   contador.innerHTML = `
     O maior evento de tecnologia do Brasil começa em 
-    <strong>${dias}d</strong> 
-    <strong>${horas}h</strong> 
-    <strong>${minutos}m</strong> 
+    <strong>${dias}d</strong>
+    <strong>${horas}h</strong>
+    <strong>${minutos}m</strong>
     <strong>${segundos}s</strong>
   `;
 }
 
 atualizaContador();
 setInterval(atualizaContador, 1000);
-=======
-const dataDoEvento = new Date("Dec 06, 2025 19:00:00");
-const timeStampDoEvento = dataDoEvento.getTime();
-
-const contaAsHoras = setInterval(function() {
-  const agora = new Date();
-  const timeStampAtual = agora.getTime();
-
-  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
-
-  const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
-  const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60));
-  const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60)) / (1000));
-
-  document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s `;
-
-  if (distanciaAteOEvento < 0) {
-    clearInterval(contaAsHoras); {
-      document.getElementById('contador').innerHTML = 'Evento expirado';
-    }
-  }
-}, 1000)
-
->>>>>>> d959810931747defa0f1eed7bdc9ab2aa4a51036
